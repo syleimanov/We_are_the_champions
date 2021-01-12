efa89f3f3f4bb858956273b6656e161af47788f7
VLADIMIR SYLEIMANOV <bannabels@gmail.com>
2021-01-12 10:52:52 +0300

    Initial Commit

---
+++ We_are_the_champions/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  We_are_the_champions
+//
+//  Created by Vladimir Syleimanov on 12.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
