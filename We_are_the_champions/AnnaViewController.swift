447c8fc3edced9ddaa4d57c6aee8e5e22c20cfb2
anna-elay <75258936+anna-elay@users.noreply.github.com>
2021-01-12 11:47:36 +0300

    AnnaVC

---
+++ We_are_the_champions/AnnaViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AnnaViewController.swift
+//  We_are_the_champions
+//
+//  Created by Anna on 12.01.2021.
+//
+
+import UIKit
+
+class AnnaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
